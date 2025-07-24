ac186dbcfb6453b0671e9757b80a306f4dd63d7f
intelligent-username <108492855+intelligent-username@users.noreply.github.com>
2025-07-24 14:50:52 -0400

    Trying again THIS TIME FOR .msi. So close yet so far from being done

--- frontend/src-tauri/scripts/install_python.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-python --version >nul 2>&1
-if errorlevel 1 (
-    echo Python is not installed. Installing Python...
-    powershell -Command "Start-Process 'https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe' -ArgumentList '/quiet InstallAllUsers=1 PrependPath=1' -Wait"
-    echo Python installed successfully.
-) else (
-    echo Python is already installed.
-)
